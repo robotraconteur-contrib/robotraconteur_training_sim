f8342ccc3f6a5e2718706b0c43e9ca48e44621a9
John Wason <wason@wasontech.com>
2024-03-04 16:52:17 -0500

    Use cd /d

--- gz_example/create/run_sim.bat
+++ gz_example/create/run_sim.bat
@@ -4,7 +4,7 @@ setlocal
 
 set GAZEBO_MODEL_PATH=%GAZEBO_MODEL_PATH%;%~dp0\..\models
 
-cd %~dp0
+cd /d %~dp0
 if %errorlevel% neq 0 exit /b %errorlevel%
 rem start cmd /c gazebo --verbose create_world.world -s gazebo_robotraconteur_server_plugin.dll --robotraconteur-server-tcp-port=11346
 gazebo --verbose create_world.world -s gazebo_robotraconteur_server_plugin.dll --robotraconteur-server-tcp-port=11346
