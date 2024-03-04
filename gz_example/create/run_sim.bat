61a6a93106038086e0accbd99b660ff3f46c4c7f
John Wason <wason@wasontech.com>
2024-03-04 18:00:08 -0500

    Use drekar launch for create sim

--- gz_example/create/run_sim.bat
+++ gz_example/create/run_sim.bat
@@ -2,10 +2,6 @@
 
 setlocal
 
-set GAZEBO_MODEL_PATH=%GAZEBO_MODEL_PATH%;%~dp0\..\models
-
 cd /d %~dp0
-if %errorlevel% neq 0 exit /b %errorlevel%
-rem start cmd /c gazebo --verbose create_world.world -s gazebo_robotraconteur_server_plugin.dll --robotraconteur-server-tcp-port=11346
-gazebo --verbose create_world.world -s gazebo_robotraconteur_server_plugin.dll --robotraconteur-server-tcp-port=11346
-if %errorlevel% neq 0 exit /b %errorlevel%
+
+python -m drekar_launch --config-j2=drekar-launch.yaml.j2 --gui
